2147185bae4f49771d2556db5cb61e6728743f00
Iván Luengo Carretero <ivan-mac@MacBook-Pro-de-Ivan.local>
2017-10-08 20:10:09 +0200

    Create lab1C

---
+++ lab01/lab1C.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This program is pretty straight forward:
+#   -Prompts the user with a password login
+#   -Gets the password from stdin with scanf as %d
+#   -Compares to 0x149a (5247)
+#   -If equal promts the user with a shell
