e7118df70f3c0441ba37be7ebde98290a4666bbb
Ivan Luengo <evilgroot@gmail.com>
2017-10-09 13:38:58 +0200

    #!/bin/bash
    # This program is pretty straight forward:
    #   -Prompts the user with a password login
    #   -Gets the password from stdin with scanf as %d
    #   -Compares to 0x149a (5274)
    #   -If equal promts the user with a shell
    
    # Knowing the password, the crack is a piece of cake
    passfile="/tmp/lab1B.pass"
    (python -c 'print 0x149a'; echo "cat /home/lab1B/.pass > $passfile") | /levels/lab01/lab1C
    echo "Password stored in $passfile"

--- lab01/lab1C.sh
+++ lab01/lab1C.sh
@@ -3,5 +3,10 @@
 # This program is pretty straight forward:
 #   -Prompts the user with a password login
 #   -Gets the password from stdin with scanf as %d
-#   -Compares to 0x149a (5247)
+#   -Compares to 0x149a (5274)
 #   -If equal promts the user with a shell
+
+# Knowing the password, the crack is a piece of cake
+passfile="/tmp/lab1B.pass"
+(python -c 'print 0x149a'; echo "cat /home/lab1B/.pass > $passfile") | /levels/lab01/lab1C
+echo "Password stored in $passfile"
